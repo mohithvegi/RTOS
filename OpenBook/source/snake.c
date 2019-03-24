8ad179f42d33ea6a6b434bf8ad163aeada9d07d4
Mohith Vegi <mohithvegi@gmail.com>
2019-03-25 00:18:40 +0530

    Update snake.c

--- OpenBook/source/snake.c
+++ OpenBook/source/snake.c
@@ -1,306 +1,170 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <sys/types.h> 
 #include <pthread.h>
-#include <semaphore.h>
+#include <time.h>
 #include <sys/time.h>
 
-// Declaration of thread condition variables.
 pthread_cond_t cond1  = PTHREAD_COND_INITIALIZER;
 pthread_cond_t cond2  = PTHREAD_COND_INITIALIZER;
 pthread_cond_t cond3  = PTHREAD_COND_INITIALIZER;
 pthread_cond_t cond4  = PTHREAD_COND_INITIALIZER;
+pthread_cond_t cond5  = PTHREAD_COND_INITIALIZER;
 
-// mutex which we are going to use avoid race condition.
-pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t lock1 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t lock2 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t lock3 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t lock4 = PTHREAD_MUTEX_INITIALIZER;
 
-// done is a global variable which decide which waiting thread should be scheduled.
-int done = 1;
-
+int TRUE = 0;
 struct timeval start, stop;
 
-// Thread function - 1
-void * player1(void *n){
+void *player1(void *arg){
+	// printf("Player1 turn.\n");
 	while(1){
-		static int move = 0;
-		static int state = 0;
-
-		// acquire a lock.
-		pthread_mutex_lock(&lock);
-
-        if (done != (int)*(int*)n) {
-                // value of done and n is not equal, hold wait lock on condition varible
-            if ((int)*(int*)n == 1) {
-                pthread_cond_wait(&cond1, &lock);
-            }
-            else if ((int)*(int*)n == 2) {
-                pthread_cond_wait(&cond2, &lock);
-            }
-            else if ((int)*(int*)n == 3) {
-                pthread_cond_wait(&cond3, &lock);
-            }
-            else {
-                pthread_cond_wait(&cond4, &lock);
-            }
-        }
-
+		static int move1 = 0;
 		int dice;
+		pthread_mutex_lock(&lock1);
+		pthread_cond_wait(&cond1, &lock1);
 		dice = rand() % 6 + 1;
-		if(move<100){
-			move = move + dice;
+		if(move1 < 100){
+			move1 = move1 + dice;
 		}
-
-		if(move==100){
-			printf("player1 wins\n");
+		if(move1 == 100){
+			printf("Winner : Player1\n");
 			gettimeofday(&stop, NULL);
 			printf("%lu\n", (stop.tv_sec - start.tv_sec)*1000000 + (stop.tv_usec - start.tv_usec));
+			// TRUE = 1;
+			// pthread_exit(NULL);
 			exit(0);
 		}
-
-		if(move>100){
-            move = move - dice;
+		if(move1 > 100){
+			move1 = move1 - dice;
 		}
-
-		// Now time to schedule next thread accordingly using pthread_cond_signal().
-		if (done == 4) {
-            done = 1;
-            pthread_cond_signal(&cond1);
-        }
-        else if(done == 1) {
-            done = 2;
-            pthread_cond_signal(&cond2);
-        }
-        else if (done == 2) {
-            done = 3;
-            pthread_cond_signal(&cond3);
-        }
-        else if (done == 3) {
-            done = 4;
-            pthread_cond_signal(&cond4);
-        }
-
-        // Finally release mutex
-        pthread_mutex_unlock(&lock);
-
+		pthread_cond_signal(&cond5);
+		pthread_mutex_unlock(&lock1);
 	}
-
-	return NULL;
 }
 
-
-// Thread function - 2
-void * player2(void *n){
+void *player2(void *arg){
+	// printf("Player2 turn.\n");
 	while(1){
-		static int move = 0;
-		static int state = 0;
-
-		pthread_mutex_lock(&lock);
-
-        if (done != (int)*(int*)n) {
-                // value of done and n is not equal, hold wait lock on condition varible
-            if ((int)*(int*)n == 1) {
-                pthread_cond_wait(&cond1, &lock);
-            }
-            else if ((int)*(int*)n == 2) {
-                pthread_cond_wait(&cond2, &lock);
-            }
-            else if ((int)*(int*)n == 3) {
-                pthread_cond_wait(&cond3, &lock);
-            }
-            else {
-                pthread_cond_wait(&cond4, &lock);
-            }
-        }
-
+		static int move2 = 0;
 		int dice;
+		pthread_mutex_lock(&lock2);
+		pthread_cond_wait(&cond2, &lock2);
 		dice = rand() % 6 + 1;
-		if(move<100){
-			move = move + dice;
+		if(move2 < 100){
+			move2 = move2 + dice;
 		}
-
-		if(move==100){
-			printf("player2 wins\n");
+		if(move2 == 100){
+			printf("Winner : Player2\n");
 			gettimeofday(&stop, NULL);
 			printf("%lu\n", (stop.tv_sec - start.tv_sec)*1000000 + (stop.tv_usec - start.tv_usec));
+			// TRUE = 1;
+			// pthread_exit(NULL);
 			exit(0);
 		}
-
-		if(move>100){
-            move = move - dice;
+		if(move2 > 100){
+			move2 = move2 - dice;
 		}
-
-		if (done == 4) {
-            done = 1;
-            pthread_cond_signal(&cond1);
-        }
-        else if(done == 1) {
-            done = 2;
-            pthread_cond_signal(&cond2);
-        }
-        else if (done == 2) {
-            done = 3;
-            pthread_cond_signal(&cond3);
-        }
-        else if (done == 3) {
-            done = 4;
-            pthread_cond_signal(&cond4);
-        }
-
-        // Finally release mutex
-        pthread_mutex_unlock(&lock);
-
+		pthread_cond_signal(&cond5);
+		pthread_mutex_unlock(&lock2);
 	}
-
-	return NULL;
 }
 
-
-// Thread function - 3
-void * player3(void *n){
+void *player3(void *arg){
+	// printf("Player3 turn.\n");
 	while(1){
-		static int move = 0;
-		static int state = 0;
-
-		pthread_mutex_lock(&lock);
-
-        if (done != (int)*(int*)n) {
-                // value of done and n is not equal, hold wait lock on condition varible
-            if ((int)*(int*)n == 1) {
-                pthread_cond_wait(&cond1, &lock);
-            }
-            else if ((int)*(int*)n == 2) {
-                pthread_cond_wait(&cond2, &lock);
-            }
-            else if ((int)*(int*)n == 3) {
-                pthread_cond_wait(&cond3, &lock);
-            }
-            else {
-                pthread_cond_wait(&cond4, &lock);
-            }
-        }
-
+		static int move3 = 0;
 		int dice;
+		pthread_mutex_lock(&lock3);
+		pthread_cond_wait(&cond3, &lock3);
 		dice = rand() % 6 + 1;
-		if(move<100){
-			move = move + dice;
+		if(move3 < 100){
+			move3 = move3 + dice;
 		}
-
-		if(move==100){
-			printf("player3 wins\n");
+		if(move3 == 100){
+			printf("Winner : Player3\n");
 			gettimeofday(&stop, NULL);
 			printf("%lu\n", (stop.tv_sec - start.tv_sec)*1000000 + (stop.tv_usec - start.tv_usec));
+			// pthread_exit(NULL);
+			// TRUE = 1;
 			exit(0);
 		}
-
-		if(move>100){
-            move = move - dice;
+		if(move3 > 100){
+			move3 = move3 - dice;
 		}
-
-		if (done == 4) {
-            done = 1;
-            pthread_cond_signal(&cond1);
-        }
-        else if(done == 1) {
-            done = 2;
-            pthread_cond_signal(&cond2);
-        }
-        else if (done == 2) {
-            done = 3;
-            pthread_cond_signal(&cond3);
-        }
-        else if (done == 3) {
-            done = 4;
-            pthread_cond_signal(&cond4);
-        }
-
-        // Finally release mutex
-        pthread_mutex_unlock(&lock);
-
+		pthread_cond_signal(&cond5);
+		pthread_mutex_unlock(&lock3);
 	}
-
-	return NULL;
 }
 
-
-// Thread function - 4
-void * player4(void *n){
+void *player4(void *arg){
+	// printf("Player4 turn.\n");
 	while(1){
-		static int move = 0;
-		static int state = 0;
-
-		pthread_mutex_lock(&lock);
-
-        if (done != (int)*(int*)n) {
-                // value of done and n is not equal, hold wait lock on condition varible
-            if ((int)*(int*)n == 1) {
-                pthread_cond_wait(&cond1, &lock);
-            }
-            else if ((int)*(int*)n == 2) {
-                pthread_cond_wait(&cond2, &lock);
-            }
-            else if ((int)*(int*)n == 3) {
-                pthread_cond_wait(&cond3, &lock);
-            }
-            else {
-                pthread_cond_wait(&cond4, &lock);
-            }
-        }
-
+		static int move4 = 0;
 		int dice;
+		pthread_mutex_lock(&lock4);
+		pthread_cond_wait(&cond4, &lock4);
 		dice = rand() % 6 + 1;
-		if(move<100){
-			move = move + dice;
+		if(move4 < 100){
+			move4 = move4 + dice;
 		}
-
-		if(move==100){
-			printf("player4 wins\n");
+		if(move4 == 100){
+			printf("Winner : Player4\n");
 			gettimeofday(&stop, NULL);
 			printf("%lu\n", (stop.tv_sec - start.tv_sec)*1000000 + (stop.tv_usec - start.tv_usec));
+			// TRUE = 1;
+			// pthread_exit(NULL);
 			exit(0);
 		}
-
-		if(move>100){
-            move = move - dice;
+		if(move4 > 100){
+			move4 = move4 - dice;
 		}
-
-		if(done == 4) {
-            done = 1;
-            pthread_cond_signal(&cond1);
-        }
-        else if(done == 1) {
-            done = 2;
-            pthread_cond_signal(&cond2);
-        }
-        else if (done == 2) {
-            done = 3;
-            pthread_cond_signal(&cond3);
-        }
-        else if (done == 3) {
-            done = 4;
-            pthread_cond_signal(&cond4);
-        }
-
-        // Finally release mutex
-        pthread_mutex_unlock(&lock);
-
+		pthread_cond_signal(&cond5);
+		pthread_mutex_unlock(&lock4);
 	}
-
-	return NULL;
 }
 
-int main(){
-    //struct timeval start, stop;
-	pthread_t  tid1, tid2, tid3, tid4;
-    int n1 = 1, n2 = 2, n3 = 3, n4 = 4;
-    srand(time(0));
-    pthread_create(&tid1, NULL, player1, (void *)&n1);
-    pthread_create(&tid2, NULL, player2, (void *)&n2);
-    pthread_create(&tid3, NULL, player3, (void *)&n3);
-    pthread_create(&tid4, NULL, player4, (void *)&n4);
+int main(void){
+	pthread_t tid1, tid2, tid3, tid4;
+	srand(time(0));
+	if( pthread_create(&tid1, NULL, player1, NULL) != 0 )
+        printf("Failed to create thread1\n");
+
+	if( pthread_create(&tid2, NULL, player2, NULL) != 0 )
+        printf("Failed to create thread2\n");
+
+	if( pthread_create(&tid3, NULL, player3, NULL) != 0 )
+        printf("Failed to create thread3\n");
+
+	if( pthread_create(&tid4, NULL, player4, NULL) != 0 )
+        printf("Failed to create thread4\n");
+
     gettimeofday(&start, NULL);
 
-    // infinite loop to avoid exit of a program/process
-    while(1);
+    while(1){
+    	pthread_cond_signal(&cond1);
+    	pthread_mutex_unlock(&lock1);
+    	pthread_mutex_lock(&lock1);
+    	pthread_cond_wait(&cond5, &lock1);
+
+    	pthread_cond_signal(&cond2);
+    	pthread_mutex_unlock(&lock2);
+    	pthread_mutex_lock(&lock2);
+    	pthread_cond_wait(&cond5, &lock2);
+
+    	pthread_cond_signal(&cond3);
+    	pthread_mutex_unlock(&lock3);
+    	pthread_mutex_lock(&lock3);
+    	pthread_cond_wait(&cond5, &lock3);
+
+    	pthread_cond_signal(&cond4);
+    	pthread_mutex_unlock(&lock4);
+    	pthread_mutex_lock(&lock4);
+    	pthread_cond_wait(&cond5, &lock4);
+    }
 
     return 0;
 }
